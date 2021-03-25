594d612999d7644f1b8c9a6d25395d59dc7ea902
Steven <stevenlambregts@gmail.com>
2021-03-25 17:57:24 +0100

    Update the get BTC button after having enough bitcoin

--- currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/BitcoinFragment.kt
+++ currencyii/src/main/java/nl/tudelft/trustchain/currencyii/ui/bitcoin/BitcoinFragment.kt
@@ -112,13 +112,8 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
 
         // If the user has too little bitcoin, he can press the button to get more,
         // the amount that is added is hardcoded on the server somewhere.
-        @Suppress("DEPRECATION")
-        if (walletManager.kit.wallet().getBalance(Wallet.BalanceType.ESTIMATED).isGreaterThan(Coin.parseCoin("10"))) {
-            add_btc.isClickable = false
-            add_btc.background.setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY)
-            add_btc.setOnClickListener {
-                Toast.makeText(this.requireContext(), "You already have enough bitcoin don't you think?", Toast.LENGTH_SHORT).show()
-            }
+        if (checkTooMuchBitcoin()) {
+            disableGetBitcoinButton()
         } else {
             add_btc.setOnClickListener {
                 if (!getBitcoinPressed) {
@@ -128,6 +123,20 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
                     Toast.makeText(this.requireContext(), "You are already given an amount of BTC, please wait a little longer", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+    }
+
+    private fun checkTooMuchBitcoin(): Boolean {
+        val walletManager = WalletManagerAndroid.getInstance()
+        return walletManager.kit.wallet().getBalance(Wallet.BalanceType.ESTIMATED).isGreaterThan(Coin.parseCoin("10"))
+    }
+
+    @Suppress("DEPRECATION")
+    private fun disableGetBitcoinButton() {
+        add_btc.isClickable = false
+        add_btc.background.setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY)
+        add_btc.setOnClickListener {
+            Toast.makeText(this.requireContext(), "You already have enough bitcoin don't you think?", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -169,6 +178,10 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
         yourPublicHex.text = walletManager.networkPublicECKeyHex()
         protocolKey.text = walletManager.protocolAddress().toString()
 
+        if (checkTooMuchBitcoin()) {
+            disableGetBitcoinButton()
+        }
+
         requireActivity().invalidateOptionsMenu()
     }
 
@@ -186,6 +199,7 @@ class BitcoinFragment : BaseFragment(R.layout.fragment_bitcoin),
                 Log.i("Coin", "Successfully added bitcoin to $address")
                 Toast.makeText(context, "Successfully added bitcoin", Toast.LENGTH_SHORT).show()
                 getBitcoinPressed = false
+                Thread.sleep(500)
                 this.refresh(true)
             },
             { error ->
